0da67c498a45b4302b14a77dba69b4ee23769331
Ben Chapman <hello@benchapman.ie>
2020-03-01 15:43:00 +0000

    Backfill tests for existing item types
    * Rename GildedRose to UpdateItems
    * Split out UpdateItem for simpler refactoring later

--- go/gilded-rose_test.go
+++ go/gilded-rose_test.go
@@ -6,15 +6,87 @@ import (
 )
 
 var _ = Describe("Main", func() {
-	It("should have items", func() {
-		Expect(len(items)).To(BeNumerically(">", 0))
-	})
-
 	It("should lower the quality each day", func() {
 		testItems := []Item{
 			Item{"Test Item", 10, 20},
 		}
-		GildedRose(testItems)
+		UpdateItems(testItems)
 		Expect(testItems[0].quality).To(Equal(19))
+	})
+
+	It("should lower the sell in each day", func() {
+		testItems := []Item{
+			Item{"Test Item", 10, 20},
+		}
+		UpdateItems(testItems)
+		Expect(testItems[0].sellIn).To(Equal(9))
+	})
+
+	It("should lower the quality by 2 when sellin day passed", func() {
+		testItems := []Item{
+			Item{"Test Item", 0, 20},
+		}
+		UpdateItems(testItems)
+		Expect(testItems[0].quality).To(Equal(18))
+	})
+
+	It("should not give items a negative quality", func() {
+		testItems := []Item{
+			Item{"Test Item", 0, 0},
+		}
+		UpdateItems(testItems)
+		Expect(testItems[0].quality).To(Equal(0))
+	})
+
+	It("should increase the quality of Aged Brie as it ages", func() {
+		testItems := []Item{
+			Item{"Aged Brie", 2, 20},
+		}
+		UpdateItems(testItems)
+		Expect(testItems[0].quality).To(Equal(21))
+	})
+
+	It("should not increase the quality of an item above 50", func() {
+		testItems := []Item{
+			Item{"Aged Brie", 2, 50},
+		}
+		UpdateItems(testItems)
+		Expect(testItems[0].quality).To(Equal(50))
+	})
+
+	It("does not have to sell Sulfuras or decrease in quality", func() {
+		testItems := []Item{
+			Item{"Sulfuras, Hand of Ragnaros", 0, 80},
+		}
+		UpdateItems(testItems)
+		Expect(testItems[0].sellIn).To(Equal(0))
+		Expect(testItems[0].quality).To(Equal(80))
+	})
+
+	Context("Backstage Passes", func() {
+		It("increases in quality as sellin day approaches", func() {
+			testItems := []Item{
+				Item{"Backstage passes to a TAFKAL80ETC concert", 20, 5},
+			}
+			UpdateItems(testItems)
+			Expect(testItems[0].quality).To(Equal(6))
+		})
+
+		It("increases in quality by 2 when as sellin day is 10 or less", func() {
+			testItems := []Item{
+				Item{"Backstage passes to a TAFKAL80ETC concert", 6, 5},
+			}
+			UpdateItems(testItems)
+			Expect(testItems[0].quality).To(Equal(7))
+		})
+
+		It("increases in quality by 3 when as sellin day is 5 or less", func() {
+			testItems := []Item{
+				Item{"Backstage passes to a TAFKAL80ETC concert", 4, 10},
+			}
+			UpdateItems(testItems)
+			Expect(testItems[0].quality).To(Equal(13))
+		})
+
 	})
 })
